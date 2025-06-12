7186efd89dd3e42691f16363da863161c03e285f
Felipe Sampaio <03.felipesampaio@gmail.com>
2025-06-11 21:52:09 -0300

    Refactor Gmail watcher functionality into GmailAPIService class

--- src/cloud_functions/gmail_bills_and_statements_message_watcher/gmail.py
+++ src/cloud_functions/gmail_bills_and_statements_message_watcher/gmail.py
@@ -1,6 +1,17 @@
-def refresh_gmail_watcher(gmail_service, user_id, topic_name, labelsIds=None):
-    """Call the Gmail API to watch for changes in the user's mailbox."""
-    return {
-        "expiration": "1748310342",
-        "historyId": "1234567890",
-    }
+
+
+class GmailAPIService:
+    def __init__(self, service):
+        self.service = service
+
+    def watch(self, user_id, topic_name, labelsIds=None):
+        """Call the Gmail API to watch for changes in the user's mailbox."""
+        return {
+            "expiration": "1748310342",
+            "historyId": "1234567890",
+        }
+
+    def stop_watching(self, user_id):
+        """Call the Gmail API to stop watching for changes in the user's mailbox."""
+        # return self.service.users().stop(userId=user_id).execute()
+        return None
