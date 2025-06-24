5d9fab68f93e23647d91fea2350c7372dcf09674
Felipe Sampaio <03.felipesampaio@gmail.com>
2025-06-24 16:55:20 -0300

    Refactor: Enhance logging and error handling in refresh_watch and download_statements_and_bills_from_message_on_topic functions

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import functions_framework
 from cloudevents.http.event import CloudEvent
 from google.cloud import firestore
 
-
+from . import setup_logger
 from . import setup_env
 from . import gcloud_utils
 from . import oauth_utils
@@ -16,15 +16,30 @@ from . import firestore_service
 from . import gmail_service
 from . import message_handler
 
+setup_logger.setup_logging(os.getenv("ENVIRON", "DEV"), os.getenv("LOG_LEVEL", "INFO"))
+
+logger.info("Initializing function environment.")
+logger.info("Looking up for environment YAML.")
 if Path("env.yaml").exists():
+    logger.info("Found env.yaml file locally. Loading it.")
     env_data = yaml.safe_load(Path("env.yaml").read_text("utf8"))
 else:
+    logger.info("Did not find env.yaml. Fetching file from Cloud Secrets.")
     env_data = gcloud_utils.get_secret_yaml(os.getenv("CONFIG_YAML_SECRET_NAME"))
 
+logger.info("Sending env variables for validation.")
 settings = setup_env.load_and_validate_environment(env_data)
 
+logger.info(
+    "Connecting to firestore database '{database}'",
+    database=settings.FIRESTORE_DATABASE_ID,
+)
 db = firestore_service.FirestoreService(
     firestore.Client(database=settings.FIRESTORE_DATABASE_ID)
+)
+logger.info(
+    "Successfully connected to firestore '{database}'",
+    database=settings.FIRESTORE_DATABASE_ID,
 )
 
 bucket = gcloud_utils.get_bucket(settings.ATTACHMENT_DESTINATION_BUCKET)
@@ -86,28 +101,41 @@ def oauth_callback_function(request):
 
 @functions_framework.http
 def refresh_watch(request):
-    users_refreshed = []
-    users_failed = {}
+    logger.info("Starting execution of refresh_watch")
+    users_refreshed = 0
+    users_failed = 0
 
     for user_ref in db.get_all_users_iterator():
+        user_logger = logger.bind(user_id=user_ref.id)
+        user_logger.info("Refreshing watch")
         try:
-            user_data = user_ref.to_dict()
+            user_data = db.get_user_data(user_ref.id)
+
+            if not user_data:
+                raise ValueError(
+                    f"User '{user_ref.id}' data was not found. Probably was deleted after the start of this function"
+                )
 
             if not user_data.get("authTokens"):
-                logger.warning(
-                    f"There is no tokens for user '{user_ref.id}'. Skipping..."
+                user_logger.warning(
+                    "Found no tokens. Skipping...",
                 )
                 continue
+
+            # Getting credentials and building Gmail Service for user
 
             creds = oauth_utils.refresh_user_credentials(
                 user_data["authTokens"], settings.OAUTH_SCOPES
             )
             db.set_user_auth_tokens(user_ref.id, creds)
             gmail = gmail_service.GmailService(
-                gmail_service.build_user_gmail_service(creds)
+                gmail_service.build_user_gmail_service(creds), user_ref.id
             )
+
+            # Calling watch() for user
             watch_res = gmail.watch(settings.PUBSUB_TOPIC)
 
+            # Writing watch response to database
             with db.client.transaction() as transaction:
                 db.update_user_last_watch(
                     transaction,
@@ -117,11 +145,21 @@ def refresh_watch(request):
                     watch_res["historyId"],
                 )
 
-            users_refreshed.append(user_ref.id)
-        except Exception as e:
-            users_failed[user_ref.id] = {"errorMessage": str(e)}
+            users_refreshed += 1
+            user_logger.info(
+                "Refreshed watch",
+                watch=watch_res,
+            )
+        except Exception:
+            user_logger.exception(
+                "Failed to refresh watch",
+            )
+            users_failed += 1
 
-    return {"usersRefreshed": users_refreshed, "failed": users_failed}, 200
+    if users_failed:
+        return users_failed, 500
+
+    return {"usersRefreshed": len(users_refreshed)}, 200
 
 
 @functions_framework.cloud_event
@@ -138,7 +176,7 @@ def download_statements_and_bills_from_message_on_topic(cloud_event: CloudEvent)
 
     topic_message = gcloud_utils.decode_topic_message(data)
     logger.info(f"Received message: {topic_message}")
-    
+
     event_email = topic_message["emailAddress"]
     event_history_id = topic_message["historyId"]
 
@@ -154,8 +192,7 @@ def download_statements_and_bills_from_message_on_topic(cloud_event: CloudEvent)
         )
         db.set_user_auth_tokens(event_email, creds)
         gmail = gmail_service.GmailService(
-            gmail_service.build_user_gmail_service(creds),
-            event_email
+            gmail_service.build_user_gmail_service(creds), event_email
         )
         logger.info(f"Built Gmail service for user '{event_email}'")
     except Exception as e:
@@ -185,18 +222,18 @@ def download_statements_and_bills_from_message_on_topic(cloud_event: CloudEvent)
         for message in new_messages:
             message_content = gmail.fetch_message_by_id(message["id"], "full")
             message_subject = gmail.get_message_subject(message_content)
-            
+
             if message_subject not in SUBJECTS:
                 logger.info(
                     f"Subject '{message_subject}' is NOT on watched subject list. Skipping..."
                 )
             else:
                 logger.info(
-                    f"Message '{message["id"]}' has a desired subject '{message_subject}'. Getting it attachments."
+                    f"Message '{message['id']}' has a desired subject '{message_subject}'. Getting it attachments."
                 )
-                
+
                 attachment_handlers = SUBJECTS[message_subject]
-                
+
                 for handler in attachment_handlers:
                     attachments = gmail.download_attachments_with_condition(
                         message_content, handler.filter
@@ -204,10 +241,12 @@ def download_statements_and_bills_from_message_on_topic(cloud_event: CloudEvent)
 
                     for attachment in attachments:
                         handler.run(message_content, attachment)
-                    
-            logger.info(f"Updating history_id_checkpoint from '{history_id_checkpoint}' to '{message_content["historyId"]}'")
+
+            logger.info(
+                f"Updating history_id_checkpoint from '{history_id_checkpoint}' to '{message_content['historyId']}'"
+            )
             history_id_checkpoint = int(message_content["historyId"])
-        
+
     except Exception as e:
         logger.error(f"Failed to handle messages for user '{event_email}'. Error: {e}")
         raise e
@@ -215,8 +254,7 @@ def download_statements_and_bills_from_message_on_topic(cloud_event: CloudEvent)
         # TODO add correct transaction here. The current one is failing on get methods
         if history_id_checkpoint != user_last_history_id:
             db.update_user_last_history_id(event_email, history_id_checkpoint)
-    
-    
+
     return (
         f"Successfully updated history to '{topic_message['historyId']}' for user '{event_email}'",
         200,
