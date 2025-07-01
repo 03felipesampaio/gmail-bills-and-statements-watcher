c7cc6997226284fa7b306df039dc2ad72b93ff2e
Felipe Amorim <03.felipesampaio@gmail.com>
2025-07-01 14:39:33 -0300

    Refactor: Add build_message_handler_from_dict import for improved message handling

--- src/handler_service/__init__.py
+++ src/handler_service/__init__.py
@@ -1,3 +1,3 @@
 from .service import *  # noqa: F403
-from .message_handlers import MessageHandler, MessageAction, MessageActionDownloadLocally  # noqa: F401
+from .message_handlers import MessageHandler, MessageAction, MessageActionDownloadLocally, build_message_handler_from_dict  # noqa: F401
 
